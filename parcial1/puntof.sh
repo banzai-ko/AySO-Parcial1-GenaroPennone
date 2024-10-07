38df9cee762d0b1a2c070ff89b6c3f9383ff73bb
banzai.ko <boxko398@gmail.com>
2024-10-07 03:22:10 +0000

    puntof.sh

--- parcial1/puntof.sh
+++ parcial1/puntof.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo Mi IP Publica es: 190.31.94.112 > Filtro_avanzado.txt
-echo Mi usuario es: vagrant >> Filtro_avanzado.txt 
-echo El hash de mi usuario es: $y$j9T$YKDqIFD.esIbEj1zqtfhF1$2BxW2Gvo/GCjKPKWJ75Rb3jADWRLpZ7W9MOZmyB5RX2 >> Filtro_avanzado.txt
+echo "Mi IP Publica es: \$(curl -s ifconfig.me)" > Filtro_avanzado.txt
+echo "Mi usuario es: \$(whoami)" >> Filtro_avanzado.txt
+echo "El hash de mi usuario es: \$(sudo cat /etc/shadow | grep vagrant | awk -F \:' \{print $2}' )" >> Filtro_avanzado.txt
