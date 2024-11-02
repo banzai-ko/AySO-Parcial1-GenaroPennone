cf83f8f56dce1c4b661960c7912469ab7bb8fa4c
Garufa <108635219+banzai-ko@users.noreply.github.com>
2024-11-02 14:13:39 -0300

    Update puntoa.sh

--- parcial1/puntoa.sh
+++ parcial1/puntoa.sh
@@ -4,4 +4,4 @@
 #sudo mkdir -p /EXAMENES-UTN/alumno_{1,2,3}//parcial_{1,2,3} /EXAMENES-UTN/profesores
 
 # Corregido 2/11/24
-sudo mkdir -p EXAMENES-UTN/{{alumno_{1..3},profesores},alumno_{1..3}/parcial_{1..3}}
+sudo mkdir -p /EXAMENES-UTN/{{alumno_{1..3},profesores},alumno_{1..3}/parcial_{1..3}}
