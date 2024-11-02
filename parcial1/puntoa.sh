53ab07fdc495bb00394fe54ee5a4e41f139cd01d
banzai.ko <boxko398@gmail.com>
2024-11-02 17:08:17 +0000

    Correcciones Devolución 2/11/24

--- parcial1/puntoa.sh
+++ parcial1/puntoa.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo mkdir -p /EXAMENES-UTN/alumno_{1,2,3}//parcial_{1,2,3} /EXAMENES-UTN/profesores
+
+# Respuesta con error:
+#sudo mkdir -p /EXAMENES-UTN/alumno_{1,2,3}//parcial_{1,2,3} /EXAMENES-UTN/profesores
+
+# Corregido 2/11/24
+sudo mkdir -p EXAMENES-UTN/{{alumno_{1..3},profesores},alumno_{1..3}/parcial_{1..3}}
--- parcial1/puntod.sh
+++ parcial1/puntod.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-mkdir -p Estructura_Asimetrica/{correo,clientes}/{cartas_{1..100}} Estructura_Asimetrica/correo/{carteros_{1..10}}
+
+# Respuesta con error
+#mkdir -p Estructura_Asimetrica/{correo,clientes}/{cartas_{1..100}} Estructura_Asimetrica/correo/{carteros_{1..10}}
+
+# Respuesta Correcta Corregido 2/11/24
+ mkdir -p Estructura_Asimetrica/{{correo,clientes}/cartas_{1..100},correo/carteros_{1..10}}
